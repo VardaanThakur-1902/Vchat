2c4db6d7bec8f916c0a1e7fab8d0dc5e68f61433
Vardaan Thakur <vardaant1902@gmail.com>
2026-06-17 16:04:02 +0530

    first commit

---
+++ server/lib/utils.js
@@ -0,0 +1,8 @@
+import jwt from "jsonwebtoken";
+
+// function to generate a token for a user
+
+export const genrateToken = (userId)=>{
+    const token = jwt.sign({userId}, process.env.JWT_SECRET);
+    return token;
+}
